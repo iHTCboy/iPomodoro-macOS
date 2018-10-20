978c33b853e780c73b2eff10393b3e2c6bf01476
iHTCboy <ihetiancong@gmail.com>
2018-10-20 15:47:19 +0800

    initial Xcode project

---
+++ iPomodoro-macOS/iPomodoro-macOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  iPomodoro-macOS
+//
+//  Created by HTC on 2018/10/20.
+//  Copyright © 2018 HTC. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
